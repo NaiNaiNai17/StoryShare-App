36b88be4e114164c296971f33272cf1b5df0f3d6
NaiNaiNai17 <fowler.naimah@gmail.com>
2021-12-19 11:03:15 +0100

    added handlebars,views folder with dash and login hbs and layouts folder inside with login and main hbs

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+
+//login and landing page
+ router.get('/', (req,res)=>{
+    res.render('login')
+})
+
+//dashboard route
+router.get('/dashboard', (req,res)=>{
+    res.render('dashboard')
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const morgan = require('morgan') 
+const exphbs = require('express-handlebars')
+// const { engine } = require ('express-handlebars');
 const connectDB = require('./config/db')
 
 
 //{path:'./config/config.env'} later
 dotenv.config() 
-
-const app = express()
 connectDB()
+const app = express()
 
+//logging using morgan
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+
+//handlebars template engine
+app.engine(
+    '.hbs',
+    exphbs.engine({
+     
+      defaultLayout: 'main',
+      extname: '.hbs',
+    })
+  )
+  app.set('view engine', '.hbs')
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars')
+// app.set('views', './views')
+//routes
+app.use('/', require('./routes/index'))
 const PORT = process.env.PORT || 5000 
-app.listen(app.get('port'), () => {
+app.listen(PORT,() => {
      console.log('Server started on port' , PORT)
  })
 
